8a6535c8f8e4787ed07d1a8ea7d9edba845491f4
Christopher Gandrud <christopher.gandrud@gmail.com>
2014-08-06 18:15:37 +0100

    check

--- R/datapackage_install.R
+++ R/datapackage_install.R
@@ -24,7 +24,7 @@ datapackage_install <- function(path, load_file = NULL, full_meta = FALSE){
     }
 
     # Parse the datapackage.json file to find the resources
-    meta <- paste0(path, '/datapackage.json') %>% fromJSON()
+    meta <- paste0(path, '/datapackage.json') %>% jsonlite::fromJSON()
 
     #### Return background information to user ------------------------------- #
     meta_message <- function(field, pre_field){
