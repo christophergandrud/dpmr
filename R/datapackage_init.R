5b4c546f28d4960096675ca3e7571d9168e3ef73
Christopher Gandrud <christopher.gandrud@gmail.com>
2014-08-08 15:32:23 +0100

    minor

--- R/datapackage_init.R
+++ R/datapackage_init.R
@@ -39,27 +39,16 @@ datapackage_init <- function(df, package_name, meta = NULL,
                                     'already exists in this directory.'),
                                     call. = F)
 
-    message(paste('\n--------------------------------',
-            'Creating', name, 'datapackage in the working directory.'))
+    message(paste('\n--- Creating the', name,
+            'data package in the working directory. ---\n'))
     dir.create(name); dir.create(paste0(name, '/data'))
     dir.create(paste0(name, '/scripts'))
-
-    #---------------------- Copy source files into scripts ------------------- #
-    if (!is.null(source_cleaner)) {
-        message('Moving in the source cleaner files:')
-        for (i in 1:length(source_cleaner)){
-            new_s_name <- source_cleaner[i] %>% gsub(pattern = '(.*\\/)([^.]+)',
-                                            replacement = paste0('process_', i))
-
-            file.copy(from = source_cleaner[i],
-                to = paste0(name, '/scripts/', new_s_name))
-            message(new_s_name)
-        }
-    }
 
     #----------------------- Create/validate datapackage.json ---------------- #
     data_base_paths <- paste0('data/', name, '_data.csv')
     if (is.null(meta)){ # Create bare
+        message(paste('\nCreating barebones metadata datapackage.json\n',
+                    '- Please add additional information directly in the JSON file.\n'))
         meta_template(df, data_base_paths) %>%
         toJSON(pretty = T) %>%
         writeLines(con = paste0(name, '/datapackage.json'))
@@ -68,9 +57,34 @@ datapackage_init <- function(df, package_name, meta = NULL,
 
     }
 
+    #---------------------- Copy source files into scripts ------------------- #
+    if (!is.null(source_cleaner)) {
+        message('Moving in the source cleaner file(s):')
+        if (length(source_cleaner) == 1) {
+            new_s_name <- source_cleaner %>%
+                            gsub(pattern = '(.*\\/)([^.]+)',
+                                replacement = paste0('process'))
+
+            file.copy(from = source_cleaner,
+                to = paste0(name, '/scripts/', new_s_name))
+            message(new_s_name)
+        }
+        else if (length(source_cleaner) > 1){
+            for (i in 1:length(source_cleaner)){
+                new_s_name <- source_cleaner[i] %>%
+                                gsub(pattern = '(.*\\/)([^.]+)',
+                                    replacement = paste0('process_', i))
+
+                file.copy(from = source_cleaner[i],
+                    to = paste0(name, '/scripts/', new_s_name))
+                message(new_s_name)
+            }
+        }
+    }
 
     #--- TO-DO Validate Data Frame using testdat ----------------------------- #
 
     # Write the data file into data/ as a CSV
+    message(paste('\nSaving data frame as', data_base_paths))
     write.csv(df, file = paste0(name, '/', data_base_paths)) # CHANGE NAMING SO THAT IT DRAWS ON META
 }
